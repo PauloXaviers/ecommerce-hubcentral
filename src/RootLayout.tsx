14d87e47cefc15572b07183d5177c2d9d736a068
Pauloxaviers <paulohenriquex18@gmail.com>
2026-08-10 11:50:44 -0300

    feat(layout): add logic to automatically apply header padding to pages

--- src/RootLayout.tsx
+++ src/RootLayout.tsx
@@ -1,11 +1,23 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header/Header";
+import { useRef, useEffect, useState } from "react";
 
 const RootLayout = () => {
+  const headerRef = useRef<HTMLElement>(null);
+  const [headerHeight, setHeaderHeight] = useState(0);
+
+  useEffect(() => {
+    if (headerRef.current) {
+      setHeaderHeight(headerRef.current.offsetHeight);
+    }
+  }, []); 
+
   return (
     <>
-      <Header />
-      <Outlet />
+      <Header ref={headerRef} />
+      <div style={{ paddingTop: `${headerHeight}px` }}>
+        <Outlet />
+      </div>
     </>
   );
 };
