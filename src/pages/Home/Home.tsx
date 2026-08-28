93a923f5826040093803d5b0595a2ab0df433784
PauloXaviers <paulohenriquex18@gmail.com>
2026-08-28 16:45:55 -0300

    feat: integrate ProductsSection in Home page with product fetching

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,9 +2,19 @@ import HeroBanner from "../../components/HeroBanner/HeroBanner";
 import ImageCarousel from "../../components/ImageCarousel/ImageCarousel";
 import blackFridayBanner from "../../assets/black-friday-banner.jpeg";
 import { categoriesCarousel } from "../../data/categoriesBanner";
+import ProductsSection from "../../components/ProductsSection/ProductsSection";
 import "./Home.scss";
+import { useEffect } from "react";
+import { useProduct } from "../../context/useProduct";
 
 const Home = () => {
+  const products = useProduct((state) => state.products);
+  const getAllProducts = useProduct((state) => state.getAllProducts);
+
+  useEffect(() => {
+    getAllProducts();
+  }, [getAllProducts]);
+
   return (
     <main>
       <HeroBanner
@@ -20,6 +30,12 @@ const Home = () => {
         images={categoriesCarousel}
         title="Categorias de produtos"
         subtitle="Selecione uma categoria e veja os produtos"
+      />
+      <ProductsSection
+        products={products}
+        title="Produtos mais vendidos"
+        headingTag="h4"
+        description="Todos os produtos 50% off"
       />
     </main>
   );
