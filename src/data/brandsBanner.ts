af2079351aeacf8edab65c3ffc1a1dcc994125ba
PauloXaviers <paulohenriquex18@gmail.com>
2026-09-16 17:52:38 -0300

    chore(assets/brands): replace logos with optimized images

--- src/data/brandsBanner.ts
+++ src/data/brandsBanner.ts
@@ -1,46 +1,46 @@
-import { type Image } from "../types/categoriesImages";
-import apple from "../assets/brands/apple-logo.png";
-import calvinKlein from "../assets/brands/calvin-klein-logo.png";
-import chanel from "../assets/brands/chanel-logo.png";
-import huawei from "../assets/brands/huawei-logo.png";
-import oppo from "../assets/brands/oppo-logo.png";
-import samsung from "../assets/brands/samsung-logo.png";
+import { type Image } from '../types/categoriesImages';
+import apple from '../assets/brands/apple-logo.png';
+import calvinKlein from '../assets/brands/calvin-klein-logo.webp';
+import chanel from '../assets/brands/chanel-logo.png';
+import huawei from '../assets/brands/huawei-logo.webp';
+import oppo from '../assets/brands/oppo-logo.png';
+import samsung from '../assets/brands/samsung-logo.png';
 
 export const brandsBanner: Image[] = [
-	{
-		id: "apple",
-		url: apple,
-		alt: "Apple",
-		text: "Apple",
-	},
-	{
-		id: "calvin-klein",
-		url: calvinKlein,
-		alt: "Calvin Klein",
-		text: "Calvin Klein",
-	},
-	{
-		id: "chanel",
-		url: chanel,
-		alt: "Chanel",
-		text: "Chanel",
-	},
-	{
-		id: "huawei",
-		url: huawei,
-		alt: "Huawei",
-		text: "Huawei",
-	},
-	{
-		id: "oppo",
-		url: oppo,
-		alt: "Oppo",
-		text: "Oppo",
-	},
-	{
-		id: "samsung",
-		url: samsung,
-		alt: "Samsung",
-		text: "Samsung",
-	},
+  {
+    id: 'apple',
+    url: apple,
+    alt: 'Apple',
+    text: 'Apple',
+  },
+  {
+    id: 'calvin-klein',
+    url: calvinKlein,
+    alt: 'Calvin Klein',
+    text: 'Calvin Klein',
+  },
+  {
+    id: 'chanel',
+    url: chanel,
+    alt: 'Chanel',
+    text: 'Chanel',
+  },
+  {
+    id: 'huawei',
+    url: huawei,
+    alt: 'Huawei',
+    text: 'Huawei',
+  },
+  {
+    id: 'oppo',
+    url: oppo,
+    alt: 'Oppo',
+    text: 'Oppo',
+  },
+  {
+    id: 'samsung',
+    url: samsung,
+    alt: 'Samsung',
+    text: 'Samsung',
+  },
 ];
